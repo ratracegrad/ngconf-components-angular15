c89721e7abc4c840f49969384920c7388f0cf6df
Jennifer Bland <ratracegrad@gmail.com>
2017-04-06 07:05:43 -0600

    Updated routing

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-const app = angular.module('caranddriver', []);
+const app = angular.module('caranddriver', [  'ui.select', 'ui.router'])
+
+  // .config(function($stateProvider) {
+  //   $stateProvider
+  //
+  //     .state('home', {
+  //       url: '',
+  //       component: 'carPicker',
+  //       views: {
+  //         header: 'navigation',
+  //         body: 'carPicker'
+  //       }
+  //     })
+  //
+  //     .state('car-details', {
+  //       url: ':make/:model',
+  //       component: 'carPicker',
+  //       views: {
+  //         header: 'navigation',
+  //         body: 'carInfo'
+  //       }
+  //     })
+  //
+  //
+  // });
